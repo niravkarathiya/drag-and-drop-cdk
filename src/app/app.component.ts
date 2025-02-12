bf0333f04d6a18862b53588cf5c80bc8b94fc69a
niravkarathiya <niravk.albiorix@gmail.com>
2025-02-12 19:03:30 +0530

    clipboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 import { TestComponent } from "./test/test.component";
+import { CopyComponent } from "./copy/copy.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CdkDrag, CdkDragHandle, TestComponent],
+  imports: [CdkDrag, CdkDragHandle, TestComponent, CopyComponent],
   standalone: true
 })
 export class AppComponent {
